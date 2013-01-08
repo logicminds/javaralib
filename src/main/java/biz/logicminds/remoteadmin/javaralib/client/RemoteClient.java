93a8e608137334c7446ccb78ee7d60759db7fd1e
Corey Osman <corey@logicminds.biz>
2013-01-07 20:11:23 -0800

    inital commit with starter pom file

---
+++ src/main/java/biz/logicminds/remoteadmin/javaralib/client/RemoteClient.java
@@ -0,0 +1,10 @@
+package biz.logicminds.remoteadmin.javaralib.client;
+
+import org.springframework.web.client.RestTemplate;
+
+public abstract class RemoteClient {
+
+	public RestTemplate restTemplate = new RestTemplate();
+	
+	
+}
---
+++ src/main/java/biz/logicminds/remoteadmin/javaralib/client/RemoteClientInterface.java
@@ -0,0 +1,10 @@
+package biz.logicminds.remoteadmin.javaralib.client;
+
+public interface RemoteClientInterface {
+	
+	public void getFacts();
+	public void getNodes();
+	public void getGroups();
+	
+	
+}
